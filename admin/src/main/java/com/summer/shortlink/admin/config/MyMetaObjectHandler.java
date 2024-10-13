8fbc4b872917fdb80e6e9f2b7b8307150d025083
a <1554083380@qq.com>
2024-10-13 12:32:03 +0800

    开发用户持久层字段自动填充

---
+++ admin/src/main/java/com/summer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.summer.shortlink.admin.config;
+
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        strictUpdateFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
